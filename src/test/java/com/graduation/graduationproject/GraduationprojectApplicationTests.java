042d5eeb0c5a09f91274df0d943dff5d6e482e7f
승일 <seoung1515@naver.com>
2024-03-13 13:40:43 +0900

    set gradle & db

---
+++ src/test/java/com/graduation/graduationproject/GraduationprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graduation.graduationproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraduationprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
