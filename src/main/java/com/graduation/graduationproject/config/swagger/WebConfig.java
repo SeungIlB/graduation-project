4560b4e565430a9c946b30b77b1c135ad0fc99ce
성연준 <elwlahs60@gmail.com>
2024-03-28 13:25:10 +0900

    간단한 HTML 적용 후 적용되는것 확인

--- src/main/java/com/graduation/graduationproject/config/swagger/WebConfig.java
+++ src/main/java/com/graduation/graduationproject/config/swagger/WebConfig.java
@@ -1,9 +1,12 @@
 package com.graduation.graduationproject.config.swagger;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
